f391937ac0bc687384b6c580ce3180a26152f217
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-10-20 01:13:51 -0700

    Add pandas_test_one

--- pandas/pandas_test_one.py
+++ pandas/pandas_test_one.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # url: https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
@@ -10,6 +11,16 @@ df = pd.read_csv("iris.data", names = [
     "class"]
 )
 
-print(df.info())
-print(df.describe())
-print(df.head(10))
+marker_shapes = [".", "^", "*"]
+
+ax = plt.axes()
+for i, species in enumerate(df["class"].unique()):
+    species_data = df[df["class"] == species]
+    species_data.plot.scatter(x="sepal_length",
+                              y="sepal_width",
+                              marker=marker_shapes[i],
+                              s=100,
+                              title="Sepal Width vs Length by Species",
+                              label=species, figsize=(10, 7), ax=ax
+                              )
+plt.show()

e1d06aa94f5052167eedd43a40f004a240ffed1b
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2020-10-23 22:50:02 -0700

    Add stuff to keras

--- keras/keras_test_one.py
+++ keras/keras_test_one.py
@@ -1,8 +1,22 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from tensorflow.keras import optimizers
+import numpy as np
 
 
 model = Sequential()
-model.add(Dense(units=4000, activation='sigmoid', input_dim=3))
-model.add(Dense(units=1000, activation='sigmoid'))
-print(model.summary())
+model.add(Dense(units=7, activation='sigmoid', input_dim=7))
+model.add(Dense(units=1, activation='sigmoid'))
+
+sgd = optimizers.SGD(lr=1)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+
+np.random.seed(9)
+
+X = np.array([[1,1,1,0,1,1,1], [1,1,1,0,1,1,1], [1,1,1,0,1,1,1], [1,1,1,0,1,1,1], [1,1,0,0,0,0,0], [1,1,0,0,0,0,0], [1,1,0,0,0,0,0]])
+y = np.array([[0],[0],[0],[0],[1],[1],[1]])
+model.fit(X, y, epochs=1500, verbose=False)
+print(model.predict(X))
+
+j = np.array([[1,1,1,0,1,1,0]])
+print(model.predict(j))
